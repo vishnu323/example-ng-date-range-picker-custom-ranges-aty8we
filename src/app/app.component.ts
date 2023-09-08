9b21a35eecd24300a2275d602a83b7ad1a416253
vishnu323 <44895109+vishnu323@users.noreply.github.com>
2023-09-08 16:10:59 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component,ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ExampleHeaderComponent } from './example-header/example-header.component';
+import { MatDateRangePicker ,MatDatepickerInputEvent} from '@angular/material/datepicker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  // make ExampleHeaderComponent type available in our template:
+  readonly ExampleHeaderComponent = ExampleHeaderComponent;
+  @ViewChild('picker') datePicker: MatDateRangePicker<Date>;
+  public handleDateRangeInputClick =() =>{
+    this.datePicker.open();
+    
+  }
+
+  range = new FormGroup({
+    start: new FormControl(new Date()),
+    end: new FormControl(new Date()),
+  });
+
+  public getDateFormat = (dateString:string) =>{
+    const date = new Date(dateString);
+    const month = String(date.getMonth() + 1).padStart(2, '0'); 
+    const day = String(date.getDate()).padStart(2, '0');
+    const year = date.getFullYear();
+    const formattedDate = `${month}-${day}-${year}`;
+    return formattedDate;
+  }
+
+  public parseRange = () =>{
+    const start = this.getDateFormat(this.range.value.start);
+    const end = this.getDateFormat(this.range.value.end);
+
+    const object = {
+      "start" : start,
+      "end" : end
+    }
+    
+    return object;
+  }
+
+  calendarHandler() {
+    console.log("target")
+  }
+
+   
+}
---
+++ src/app/example-header/custom-range-panel.component.ts
@@ -0,0 +1,135 @@
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { DateAdapter } from '@angular/material/core';
+import { MatDateRangePicker } from '@angular/material/datepicker';
+import { GlobalValueService } from '../global-value.service';
+const customPresets = [
+  'Today',
+  'Yesterday',
+  'Last 7 days',
+  'Last 30 days',
+  'Last 90 days',
+  'Custom range'
+] as const; // convert to readonly tuple of string literals
+
+// equivalent to "today" | "last 7 days" | ... | "last year"
+type CustomPreset = typeof customPresets[number];
+
+@Component({
+  selector: 'app-custom-range-panel',
+  templateUrl: './custom-range-panel.component.html',
+  styleUrls: ['./custom-range-panel.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CustomRangePanelComponent<D> {
+  // list of range presets we want to provide:
+  readonly customPresets = customPresets;
+  @HostBinding('class.touch-ui')
+  readonly isTouchUi = this.picker.touchUi;
+  public todayVar: boolean = true;
+
+  constructor(
+    private dateAdapter: DateAdapter<D>,
+    private picker: MatDateRangePicker<D>,
+    private globalValueService: GlobalValueService
+    
+  ) {
+  }
+
+  updateGlobalValue(newValue: any) {
+    this.globalValueService.setGlobalValue(newValue);
+  }
+
+  // Example of getting the global value
+  getGlobalValue() {
+    return this.globalValueService.getGlobalValue();
+  }
+
+  applyClassOrStyleOnce() {
+    const ref:any = document.querySelector(`#Today`);
+    ref.style.backgroundColor = "blue"
+  }
+
+  ngAfterViewInit() {
+    if(this.getGlobalValue()){
+      const ref:any = document.querySelector(`#${this.replaceSpacesWithHyphens(this.getGlobalValue())}`);
+      ref.style.backgroundColor = "blue"
+      // this.applyClassOrStyleOnce();
+    }
+  }
+  // called when user selects a range preset:
+  selectRange(rangeName: CustomPreset): void {
+    const [start, end] = this.calculateDateRange(rangeName);
+    this.picker.select(start);
+    this.picker.select(end);
+    if(start && end){
+      this.picker.close();
+    }
+    
+  }
+ 
+  setBackGround = (id:string,type:string) =>{
+    const ref:any = document.querySelector(`#${this.replaceSpacesWithHyphens(id)}`);
+    if(ref){
+      ref.style.backgroundColor=type;
+    }
+  }
+
+
+  idSelector = (id:string) =>{
+      this.setBackGround(id,"blue");
+      this.updateGlobalValue(id);
+  }
+
+    replaceSpacesWithHyphens = (inputString:any) =>{
+      return inputString.replace(/ /g, '-');
+    }
+  
+
+  private calculateDateRange(rangeName: CustomPreset): [start: D, end: D] {
+    const today = this.today;
+    const year = this.dateAdapter.getYear(today);
+    this.idSelector(rangeName);
+    switch (rangeName) {
+      case 'Today':
+        return [today, today];
+      case 'Yesterday': {
+        const start = this.dateAdapter.addCalendarDays(today, -1);
+        return [start, today];
+      }
+      case 'Last 7 days': {
+        const start = this.dateAdapter.addCalendarDays(today, -6);
+        return [start, today];
+      }
+      case 'Last 30 days':{
+        const end = today;
+        const start = this.dateAdapter.addCalendarDays(today, -30);
+        return [start, end];
+      }
+      case 'Last 90 days': {
+        const end = today;
+        const start = this.dateAdapter.addCalendarDays(today, -90);
+        return [start, end];
+      }
+      case 'Custom range':{
+        const end = today;
+        const start = this.dateAdapter.addCalendarDays(today, -90);
+        return [null, null];
+      }
+      default:
+      console.log(" vishnu123556")
+        // exhaustiveness check;
+        // rangeName has type never, if every possible value is handled in the switch cases.
+        // Otherwise, the following line will result in compiler error:
+        // "Type 'string' is not assignable to type '[start: D, end: D]'"
+        return rangeName;
+    }
+  }
+
+  private get today(): D {
+    const today = this.dateAdapter.getValidDateOrNull(new Date());
+    if (today === null) {
+      throw new Error('date creation failed');
+    }
+    return today;
+  }
+}
