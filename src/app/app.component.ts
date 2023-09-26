56c087ef422518f563e19f89a3cc42164056733d
vishnu323 <44895109+vishnu323@users.noreply.github.com>
2023-09-27 02:43:36 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,34 @@
-import { Component,ViewChild } from '@angular/core';
-import {MatDatepicker} from '@angular/material/datepicker';
-import {FormControl} from '@angular/forms';
-
-export const MY_FORMATS = {
-  parse: {
-    dateInput: 'MM/YYYY',
-  },
-  display: {
-    dateInput: 'MM/YYYY',
-    monthYearLabel: 'MMM YYYY',
-    dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMMM YYYY',
-  },
-};
-
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatDatepicker } from '@angular/material/datepicker';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [DatePipe], // Add DatePipe as a provider
 })
 export class AppComponent {
- 
+  date = new FormControl();
 
-  date = new FormControl(new Date());
+  constructor(private datePipe: DatePipe) {}
 
   chosenYearHandler(normalizedYear: Date) {
-    const ctrlValue = this.date.value;
+    const ctrlValue = this.date.value || new Date();
     ctrlValue.setFullYear(normalizedYear.getFullYear());
     this.date.setValue(ctrlValue);
   }
 
-  chosenMonthHandler(normalizedMonth: Date,picker :MatDatepicker<Date> ) {
-    const ctrlValue = this.date.value;
+  chosenMonthHandler(normalizedMonth: Date, dp: MatDatepicker<Date>) {
+    const ctrlValue = this.date.value || new Date();
     ctrlValue.setMonth(normalizedMonth.getMonth());
     this.date.setValue(ctrlValue);
-    picker.close()
+    dp.close();
   }
 
-   
+  // Format the date for display in the input field
+  formatDate(date: Date): string {
+    return this.datePipe.transform(date, 'MM/yyyy') || '';
+  }
 }
